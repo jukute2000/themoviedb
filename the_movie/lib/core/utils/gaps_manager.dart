a5a313de248928cf0ef3a1af257fb773b4349b73
phamkhoa2505 <phamkhoa12332112@gmail.com>
2025-03-20 15:19:26 +0700

    refactor/set-up-base-and-constant-text

---
+++ the_movie/lib/core/utils/gaps_manager.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class GapsManager {
+  static SizedBox h10 = const SizedBox(height: 10);
+  static SizedBox h20 = const SizedBox(height: 20);
+  static SizedBox h30 = const SizedBox(height: 30);
+  static SizedBox h40 = const SizedBox(height: 40);
+  static SizedBox h50 = const SizedBox(height: 50);
+
+  static SizedBox w10 = const SizedBox(width: 10);
+  static SizedBox w20 = const SizedBox(width: 20);
+  static SizedBox w30 = const SizedBox(width: 30);
+  static SizedBox w40 = const SizedBox(width: 40);
+  static SizedBox w50 = const SizedBox(width: 50);
+}
