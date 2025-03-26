931e4758c0a87a7aa27ef0c02453454fd13c0dea
phamkhoa2505 <phamkhoa12332112@gmail.com>
2025-03-26 10:42:34 +0700

    feature/set-up-localize

--- the_movie/lib/main.dart
+++ the_movie/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -44,6 +45,9 @@ class MyApp extends StatelessWidget {
             splitScreenMode: true,
             builder: (context, child) {
               return MaterialApp(
+                  locale: context.locale,
+                  supportedLocales: context.supportedLocales,
+                  localizationsDelegates: context.localizationDelegates,
                   debugShowCheckedModeBanner: false,
                   theme: ThemeData(
                     colorScheme:
@@ -51,17 +55,7 @@ class MyApp extends StatelessWidget {
                     useMaterial3: true,
                   ),
                   home: DetailSearchScreen());
-            })
-        // return MultiBlocProvider(
-        //   providers: [
-        //     BlocProvider(create: (context) => DemoCubit()),
-        //   ],
-        //   child: const MaterialApp(
-        //     debugShowCheckedModeBanner: false,
-        //     title: 'Flutter Demo',
-        //     home: DemoScreen(),
-        //   ),
-        );
+            }));
   }
 }
 
--- the_movie/lib/main_dev.dart
+++ the_movie/lib/main_dev.dart
@@ -1,3 +1,4 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:the_movie/flavor/flavor_config.dart';
 import 'package:the_movie/initial/firebase_initializer.dart';
@@ -8,6 +9,12 @@ import 'main.dart';
 void main() async {
   FlavorConfig(
       flavor: Flavor.dev, values: FlavorValues.fromJson(env[Flavor.dev]!));
+  WidgetsFlutterBinding.ensureInitialized();
   await FirebaseInitializer.devInitialize();
-  runApp(const MyApp());
+  await EasyLocalization.ensureInitialized();
+  runApp(EasyLocalization(
+      supportedLocales: const [Locale('vi'), Locale('en')],
+      path: 'assets/translations',
+      fallbackLocale: const Locale('en'),
+      child: MyApp()));
 }
