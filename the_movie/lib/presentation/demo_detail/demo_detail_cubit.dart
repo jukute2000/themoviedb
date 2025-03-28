e39cec80a693f25f8d2fcd783d33d4935414de12
Thien Bao <lathienbao3119410028@gmail.com>
2025-03-28 16:21:29 +0700

    update

--- the_movie/lib/presentation/demo_detail/demo_detail_cubit.dart
+++ the_movie/lib/presentation/demo_detail/demo_detail_cubit.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:the_movie/data/models/author/author.dart';
 import 'package:the_movie/data/models/credits/external.dart';
 import 'package:the_movie/data/models/credits/combined_credit.dart/combined_credit.dart';
 import 'package:the_movie/data/models/keyword/keyword.dart';
@@ -7,20 +6,11 @@ import 'package:the_movie/data/models/media_detail/detail_media/detail_movie.dar
 import 'package:the_movie/data/models/media_detail/detail_media/detail_tv.dart';
 import 'package:the_movie/data/models/medias/movie.dart';
 import 'package:the_movie/data/models/medias/tv.dart';
-import 'package:the_movie/data/models/people/people_detail.dart';
 import 'package:the_movie/data/models/release_date/release_date.dart';
-import 'package:the_movie/data/models/search/search_companies.dart';
-import 'package:the_movie/data/models/search/search_keywords.dart';
-import 'package:the_movie/data/models/search/search_movie.dart';
-import 'package:the_movie/data/models/search/search_multi.dart';
-import 'package:the_movie/data/models/search/search_tv.dart';
 import 'package:the_movie/data/models/video/video.dart';
 import 'package:the_movie/data/repositories/people_repository.dart';
-import 'package:the_movie/data/repositories/search_repository.dart';
 import 'package:the_movie/presentation/demo_detail/demo_detail_state.dart';
 import '../../data/models/credits/credit.dart';
-import '../../data/models/search/search_collections.dart';
-import '../../data/models/search/search_people.dart';
 import '../../data/repositories/media_detail_repository.dart';
 
 class DemoDetailCubit extends Cubit<DemoDetailStateCubit> {
