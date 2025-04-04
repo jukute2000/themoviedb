62e52064179b80abb08cc0137f69bb66bef98712
Thien Bao <lathienbao3119410028@gmail.com>
2025-04-04 12:14:32 +0700

    update firebase banner

---
+++ the_movie/lib/presentation/home/bloc/banner/banner_state.dart
@@ -0,0 +1,15 @@
+abstract class BannerState {}
+
+class BannerInitial extends BannerState {}
+
+class BannerIsLoading extends BannerState {}
+
+class BannerLoaded extends BannerState {
+  final String? images;
+  BannerLoaded({required this.images});
+}
+
+class BannerError extends BannerState {
+  final String message;
+  BannerError(this.message);
+}
