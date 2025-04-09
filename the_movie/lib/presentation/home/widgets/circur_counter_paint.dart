76d1db42685b5a0004c0ba9caa79120fb881f5d8
22521618 <22521618@gm.uit.edu.vn>
2025-04-09 10:02:39 +0700

    add animation

---
+++ the_movie/lib/presentation/home/widgets/circur_counter_paint.dart
@@ -0,0 +1,137 @@
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+
+class CircleCounterAnimation extends StatefulWidget {
+  final Color circleColor;
+  final Color textColor;
+  final double size;
+  final Duration duration;
+
+  const CircleCounterAnimation({
+    Key? key,
+    this.circleColor = Colors.blue,
+    this.textColor = Colors.black,
+    this.size = 150.0,
+    this.duration = const Duration(seconds: 2),
+  }) : super(key: key);
+
+  @override
+  CircleCounterAnimationState createState() => CircleCounterAnimationState();
+}
+
+class CircleCounterAnimationState extends State<CircleCounterAnimation>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _circleAnimation;
+  late Animation<double> _counterAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this, duration: widget.duration);
+
+    _circleAnimation = Tween<double>(
+      begin: 0,
+      end: 360,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOutExpo));
+
+    _counterAnimation = Tween<double>(
+      begin: 0,
+      end: 100,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOutExpo));
+
+    _controller.addListener(() {
+      setState(() {});
+    });
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: widget.size,
+        height: widget.size,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            CustomPaint(
+              size: Size(widget.size, widget.size),
+              painter: CircleArcPainter(
+                sweepAngle: _circleAnimation.value * math.pi / 180,
+                color: widget.circleColor,
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  _counterAnimation.value.toInt().toString(),
+                  style: TextStyle(
+                    color: widget.textColor,
+                    fontSize: widget.size * 0.3,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CircleArcPainter extends CustomPainter {
+  final double sweepAngle; // Góc quét tính bằng radian
+  final Color color;
+
+  CircleArcPainter({required this.sweepAngle, required this.color});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final center = Offset(size.width / 2, size.height / 2);
+    final radius = size.width / 2 - 8;
+    final rect = Rect.fromCircle(center: center, radius: radius);
+
+    final paint = Paint()
+      ..color = color
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 8.0
+      ..strokeCap = StrokeCap.round;
+
+    final startAngle = -math.pi / 2;
+
+    canvas.drawArc(rect, startAngle, sweepAngle, false, paint);
+  }
+
+  @override
+  bool shouldRepaint(CircleArcPainter oldDelegate) {
+    return oldDelegate.sweepAngle != sweepAngle || oldDelegate.color != color;
+  }
+}
+
+class CounterAnimationScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Circle Counter Animation')),
+      body: Center(
+        child: CircleCounterAnimation(
+          circleColor: Colors.blue,
+          textColor: Colors.black,
+          size: 200.0,
+          duration: Duration(seconds: 3),
+        ),
+      ),
+    );
+  }
+}
