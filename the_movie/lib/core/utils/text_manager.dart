c81353a4cd88bc25124a54e49169649fe787752d
Thien Bao <lathienbao3119410028@gmail.com>
2025-04-09 10:28:00 +0700

    Update fonts

---
+++ the_movie/lib/core/utils/text_manager.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class TextManager {
+  //SourceSans3
+  static TextStyle textStyleLight(double size) => TextStyle(
+        fontSize: size,
+        fontFamily: 'Source',
+        fontWeight: FontWeight.w300,
+      );
+  static TextStyle textStyleRegular(double size) => TextStyle(
+        fontSize: size,
+        fontFamily: 'Source',
+        fontWeight: FontWeight.w400,
+      );
+  static TextStyle textStyleMedium(double size) => TextStyle(
+        fontSize: size,
+        fontFamily: 'Source',
+        fontWeight: FontWeight.w500,
+      );
+  static TextStyle textStyleBlod(double size) => TextStyle(
+        fontSize: size,
+        fontFamily: 'Source',
+        fontWeight: FontWeight.w700,
+      );
+  static TextStyle textStyleBlack(double size) => TextStyle(
+        fontSize: size,
+        fontFamily: 'Source',
+        fontWeight: FontWeight.w900,
+      );
+}
