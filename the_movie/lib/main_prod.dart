7ecaa43bfba83d5101806ccdc36df9672659e4ff
Thien Bao <lathienbao3119410028@gmail.com>
2025-07-03 19:50:12 +0700

    update

--- the_movie/lib/main_prod.dart
+++ the_movie/lib/main_prod.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:the_movie/initial/firebase_initializer.dart';
-
+import 'package:easy_localization/easy_localization.dart';
 import 'flavor/env.dart';
 import 'flavor/flavor_config.dart';
 import 'main.dart';
@@ -8,6 +8,12 @@ import 'main.dart';
 void main() async {
   FlavorConfig(
       flavor: Flavor.dev, values: FlavorValues.fromJson(env[Flavor.prod]!));
-  await FirebaseInitializer.prodInitialize();
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  await FirebaseInitializer.devInitialize();
+  await EasyLocalization.ensureInitialized();
+  runApp(EasyLocalization(
+      supportedLocales: const [Locale('vi'), Locale('en')],
+      path: 'assets/translations',
+      fallbackLocale: const Locale('en'),
+      child: const MyApp()));
 }
